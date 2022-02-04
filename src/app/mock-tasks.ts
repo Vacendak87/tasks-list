bc6f796304e301b0e1d09d441965ca4a37fc1af3
Emanuel <emanuel.dafonte@igt.com>
2022-02-04 16:06:26 -0300

    Servicio y Servidor de tareas listo

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -4,25 +4,25 @@ export const TASKS: Task[] = [
     {
         id: 1,
         text: "Terminar primer modulo de Angular",
-        day: 'February 6 a las 12:00',
+        day: 'Febrero 6 a las 12:00',
         reminder: true
     },
     {
         id: 2,
         text: "Clase Intro a la Ing. en Computación",
-        day: 'February 3 a las 18:00',
+        day: 'Febrero 3 a las 18:00',
         reminder: true
     },
     {
         id: 3,
         text: "Médico",
-        day: 'February 5 a las 10:00',
+        day: 'Febrero 5 a las 10:00',
         reminder: false
     },
     {
         id: 4,
         text: "Investigar sobre Boostrap",
-        day: 'February 7 a las 11:00',
+        day: 'Febrero 7 a las 11:00',
         reminder: false
     }
 ]
